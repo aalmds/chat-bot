a148faa332085719e38d8f67240cde95f01750ea
Andresa <aas10@cin.ufpe.br>
2022-08-29 16:57:49 -0300

    chore(*): updating utils

--- Utils.py
+++ Utils.py
@@ -1,5 +1,16 @@
 from typing import Final
 
+# Definindo constantes utilizadas para comunicacao cliente-servidor.
 _BUFFER_SIZE: Final[int] = 2048
 _SERVER: Final[str] = "localhost"
 _SERVER_PORT: Final[int] = 12000
+_FILE: Final[str] = "file"
+_FINISHED: Final[str] = "finished"
+
+# Funcao que escreve em um arquivo.
+def write_file_process(chunk, file):
+    try:          
+        print("Writing file...")      
+        file.write(chunk)
+    except IOError:
+        print("Error in writing file.")

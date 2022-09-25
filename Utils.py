86df0effbaf4bf7f9bbe154581f7c221cd273879
Andresa <aas10@cin.ufpe.br>
2022-09-25 20:25:30 -0300

    (wip): adding rdt

--- Utils.py
+++ Utils.py
@@ -3,12 +3,3 @@ from typing import Final
 _BUFFER_SIZE: Final[int] = 2048
 _SERVER: Final[str] = "localhost"
 _SERVER_PORT: Final[int] = 12000
-_FILE: Final[str] = "file"
-_FINISHED: Final[str] = "finished"
-
-def write_file_process(chunk, file):
-    try:          
-        print("Writing file...")      
-        file.write(chunk)
-    except IOError:
-        print("Error in writing file.")
--- client.py
+++ client.py
@@ -1,50 +1,32 @@
-import socket 
-from Utils import _SERVER, _SERVER_PORT, _BUFFER_SIZE, _FILE, _FINISHED, write_file_process
+import socket
+from Utils import _SERVER, _SERVER_PORT
+from RdtSender import RdtSender
+from RdtReceiver import RdtReceiver
 
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def define_command(command, name = ""):
-    clientSocket.sendto(command.encode(), (_SERVER, _SERVER_PORT))
-    if name != "":
-        clientSocket.sendto(name.encode(), (_SERVER, _SERVER_PORT))
-    
-def send_receive_file(chunk, file):
-    print("Sending message to server...")
-    clientSocket.sendto(chunk, (_SERVER, _SERVER_PORT))
-    print("Message sent!")
+class Client:
+    def __init__(self):
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.rdt_sender = RdtSender(self.clientSocket)
+        self.rdt_receiver = RdtReceiver(self.clientSocket)
 
-    try:
-        print("Waiting server response...")
-        message, clientAddress = clientSocket.recvfrom(_BUFFER_SIZE)
-        write_file_process(message, file)
-    except (socket.error, exc): 
-        print("Socket.error - " + exc)
+    def run(self):
+        print("The client is on!")
 
-def file_process(extension):
-    file = open("./client_files/Teste" + extension, 'rb')
-    chunk = file.read(_BUFFER_SIZE)
-    new_file = open("./client_files/received" + extension, 'wb')
+        a = 5
+        while a:
+            a -= 1
+            self.rdt_sender.send("testing", (_SERVER, _SERVER_PORT))       
 
-    while chunk:
-        send_receive_file(chunk, new_file)
-        print("Reading new chunk!")
-        chunk = file.read(_BUFFER_SIZE)
-
-    define_command(_FINISHED)
-    print("File finished!")   
-    file.close()
-    new_file.close()
-
-def client():
-    print("The client is on!")
-
-    define_command(_FILE, "TXT.txt")
-    file_process("TXT.txt")
-
-    define_command(_FILE, "PDF.pdf")
-    file_process("PDF.pdf")
-
-    clientSocket.close()
+            # message, serverAddress = self.clientSocket.recvfrom(_BUFFER_SIZE)
+            # seqnum, data = message.decode().split(',')
+            # print(f"seqnum = {seqnum}, data = {data}")
+            # if serverAddress == (_SERVER, _SERVER_PORT):
+            #     print("É igual")
+            # self.rdt_receiver.receive(seqnum, (_SERVER, _SERVER_PORT))
+        
+        self.clientSocket.close()
 
 if __name__ == "__main__":
-    client()
+    client = Client()
+    client.run()

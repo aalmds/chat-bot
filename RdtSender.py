d8fc9f1884e4835bae88edcf5530d7893d81b397
Andresa <aas10@cin.ufpe.br>
2022-09-29 21:35:06 -0300

    fix(rdt): fixing timeout approach

--- RdtSender.py
+++ RdtSender.py
@@ -1,50 +1,54 @@
+import socket
 from Utils import _BUFFER_SIZE
-from threading import Timer
 
+# Classe de abstração para a máquina de estados do protocolo de transferência confiável do transmissor.
 class RdtSender:
     def __init__(self, socket):
         self.socket = socket
         self.sequence_number = '0'
-        # Define estado de espera pela chamada de cima
+        # Definindo o estado inicial como o de espera por uma mensagem a ser transferida.
         self.waiting = True
         
+    # Função que verifica se o ack recebido é referente ao pacote que foi enviado por último.
     def __check_ack(self, ack):
         return ack == self.sequence_number
 
+    # Função que atualiza o número de sequência.
     def __update_seqnum(self):
         self.sequence_number = '0' if self.sequence_number == '1' else '1' 
-    
-    def __timeout(self, chunk, address):
-        print("Timeout!")
-        # Retransmite pacote
-        self.send(chunk, address)
 
-    def __wait_ack(self, chunk, address):
-        timer = Timer(10.0, self.__timeout, args = (chunk, address))
-        timer.start()
-        
-        ack, address = self.socket.recvfrom(_BUFFER_SIZE)
-        print("Waiting for ack", self.sequence_number)
-
-        if self.__check_ack(ack.decode()):
-            print("The ack is correct!")
-            print("_____________\n")
-            # Desliga timer
-            timer.cancel()
-            # Atualiza o numero de sequencia
-            self.__update_seqnum()
-            # Volta para o estado de espera, ou seja, pode enviar dados
-            self.waiting = True
-            
+    # Função referente ao envio da mensagem por um canal confiável.
     def send(self, chunk, address):
-        # Sai do estado de espera
         self.waiting = False
 
+        # Criando o pacote com número de sequência e dado.
         pkt = self.sequence_number + "," + chunk
-        print(f"Pkt was built with: ({self.sequence_number}, \"{chunk}\")")
 
-        self.socket.sendto(pkt.encode(), address)
-        self.__wait_ack(chunk, address)
+        while not self.waiting:
+            print(f"Sending package with: ({self.sequence_number}, \"{chunk}\")")
+            self.socket.sendto(pkt.encode(), address)
+            
+            # Ligando o temporizador
+            self.socket.settimeout(5)
+        
+            try: 
+                ack, address = self.socket.recvfrom(_BUFFER_SIZE)
+            except socket.timeout:
+                # Detectando timeout.
+                print("Timeout!\n\n")
+                continue
+                
+            if self.__check_ack(ack.decode()):
+                print("The ack is correct!\n\n")
+    
+                # Desligando o temporizador
+                self.socket.settimeout(None)
+                # Atualizando o número de sequência.
+                self.__update_seqnum()
 
+                # Voltando para o estado de espera pela transmissão de uma nova mensagem.
+                self.waiting = True
+
+    # Função que retorna se o estado atual do transmissor permite transmissão de uma nova mensagem ou ainda está em espera do ack.
     def is_waiting_call(self):
         return self.waiting

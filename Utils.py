b128d42b08378714d9ec5407675668717f97a9e1
acrucha <mcaa@cin.ufpe.br>
2022-10-29 18:37:16 -0300

    (wip): ban command #2

--- Utils.py
+++ Utils.py
@@ -9,8 +9,11 @@ _CONNECT: Final[str] = "hi, meu nome eh "
 _BYE: Final[str] = "bye"
 _LIST: Final[str] = "list"
 _INBOX: Final[str] = "@"
-_BAN: Final[str] = "ban "
+_BAN: Final[str] = "ban @"
 _DISCONNECT: Final[str] = "disconect%&%"
+
+_BAN_CONDITION: Final[str] = 2/3
+_BAN_TIMEOUT: Final[str] = 60
 
 def time():
     return datetime.now().strftime("%H:%M:%S")

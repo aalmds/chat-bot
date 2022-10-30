df3ce769d3c494a00d016696484369063a2fd3de
Andresa <aas10@cin.ufpe.br>
2022-10-30 05:17:17 -0300

    (chore): adding comments

--- client.py
+++ client.py
@@ -5,32 +5,42 @@ from rdt.RdtSender import RdtSender
 from rdt.RdtReceiver import RdtReceiver
 from threading import Thread, Lock
 
+# Classe do cliente.
 class Client:
     def __init__(self):
+        # Socket do cliente.
         self.socket_lock = Lock()
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         
+        # Atributos do rdt.
         self.rdt_sender = RdtSender(self.client_socket)
         self.rdt_receiver = RdtReceiver(self.client_socket)
 
+        # Thread responsável por enviar os pacotes do cliente para o servidor.
         self.ths = Thread(target=self.__send)
         self.ths.daemon = True
                 
+    # Função target da thread de envio de pacotes pelo cliente.
     def __send(self):
-
         while True:
+            # Obtendo a mensagem do cliente.
             message = input()
             print ("\033[A                             \033[A")
             print(colored(f'[{current_time()}] Você: {message}', attrs=["bold"]))
 
+            # Verificando se o cliente está estabelecendo uma nova conexão.
             if _CONNECT in message:
+                # Reiniciando o rdt.
                 self.rdt_sender = RdtSender(self.client_socket)
                 self.rdt_receiver = RdtReceiver(self.client_socket)
-
+            
+            # Enviando a mensagem do cliente por um canal confiável.
             if self.rdt_sender.is_waiting_call():
                 self.rdt_sender.send(message + "%&% ", (_SERVER, _SERVER_PORT))              
 
+    # Função de inicialização do cliente.
     def run(self):
+        # Inicializando a thread do send.
         self.ths.start()
 
         while True:
@@ -38,19 +48,21 @@ class Client:
                 message, server_address = self.client_socket.recvfrom(_BUFFER_SIZE)
             except socket.timeout:
                 continue
-
+            
+            # Verificando se a mensagem é de ack ou não.
             if '%&%' in message.decode():
                 seqnum, message, color = message.decode().split('%&%')
                 
+                # Recebendo a mensagem do servidor de por um protocolo confiável.
                 self.socket_lock.acquire()
                 message, color = self.rdt_receiver.receive(server_address, seqnum, message, color)
                 self.socket_lock.release()
                 
                 print(colored(str(message), color, attrs=["bold"]))         
-
             else:
                 lock = self.rdt_sender.get_lock()
                 lock.acquire()
+                # Atribuindo novo valor ao ack.
                 self.rdt_sender.set_ack(message.decode())
                 lock.release()
 
--- rdt/RdtSender.py
+++ rdt/RdtSender.py
@@ -1,20 +1,25 @@
 import time
 from threading import Lock
 
+# Classe de abstração para a máquina de estados do protocolo de transferência confiável do tranmissor.
 class RdtSender:
     def __init__(self, socket, timeout=10):
         self.socket = socket
         self.sequence_number = '0'
         self.timeout = timeout
+
         # Definindo o estado inicial como o de espera por uma mensagem a ser transferida.
         self.waiting = True
 
+        # Lock para ack.
         self.lock = Lock()
         self.ack = -1
 
+    # Função que modifica o valor do ack.
     def set_ack(self, ack):
         self.ack = ack
 
+    # Função que retorna o lock do ack.
     def get_lock(self):
         return self.lock
 
@@ -32,19 +37,31 @@ class RdtSender:
 
         # Criando o pacote com número de sequência e dado.
         pkt = self.sequence_number + "%&%" + chunk
+        
+        # Capturando o tempo inicial.
         start_time = time.time()
-
+        
+        # Enviando pacote.
         self.socket.sendto(pkt.encode(), address)
+        
         # Enquanto está no estado de espera pelo ack correto, continua retransmitindo o último pacote enviado após timeout.
         while not self.waiting:
+
+            # Verificando se houve timeout.
             if time.time() - start_time > self.timeout:
                 start_time = time.time()
+                # Reenviando o pacote.
                 self.socket.sendto(pkt.encode(), address)
                 
+            
+            # Checando se o ack está correto.
             self.lock.acquire()
             if self.__check_ack():
+                # Resetando ack.
                 self.ack = -1
+                # Atualizando número de sequências.
                 self.__update_seqnum()
+                # Modificando o estado para o de espera por chamada da aplicação para uma nova transmissão.
                 self.waiting = True
             self.lock.release()
 
